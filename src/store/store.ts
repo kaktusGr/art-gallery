c04e3f453150ce31646bac0bbf8ad8083f2be766
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-12-20 15:46:46 +0100

    Update hooks in store.ts according to documentation

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,14 @@ import { TypedUseSelectorHook, useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import ArtsSlice from "./reducers/ArtsSlice";
 
-export const setupStore = configureStore({
-    reducer: {
-        arts: ArtsSlice.reducer,
-    }
-});
+export const setupStore = () => {
+    return configureStore({
+        reducer: {
+            arts: ArtsSlice.reducer,
+        }
+    });
+};
 
-export type RootState = ReturnType<typeof setupStore.getState>;
-export const useAppDispatch: () => typeof setupStore.dispatch = useDispatch;
+export type RootState = ReturnType<ReturnType<typeof setupStore>["getState"]>;
+export const useAppDispatch: () => ReturnType<typeof setupStore>["dispatch"] = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
