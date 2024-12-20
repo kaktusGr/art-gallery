3a60e2957932461f3e338e5979d8e7140041d0c2
Ekaterina Grishina <grischina.kotya17@gmail.com>
2024-12-20 15:41:21 +0100

    Update reducer ArtsSlice by adding fetch request to get info for each id

--- src/store/reducers/ArtsSlice.ts
+++ src/store/reducers/ArtsSlice.ts
@@ -1,33 +1,75 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export interface Art {
-    id: number;
-    name: string;
+interface Art {
+    id: number
+    title: string;
+    artist: string;
+    description: string;
+    imageUrl: string;
 }
 
 interface ArtsState {
+    ids: number[];
     arts: Art[];
     isLoading: boolean;
     error: string | null;
 }
 
 const initialState: ArtsState = {
+    ids: [],
     arts: [],
     isLoading: false,
     error: null,
 };
 
-export const fetchArts = createAsyncThunk<Art[], void, { rejectValue: string }>("arts/fetchArts",
+export const fetchArtsIDs = createAsyncThunk<number[], void, { rejectValue: string }>(
+    "arts/fetchArtsIDs",
     async (_, { rejectWithValue }) => {
         try {
-            const response = await fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11", {
-                method: "GET",
-            });
-            if (!response.ok) {
-                throw new Error("Failed to fetch arts");
+            const responseIDs = await fetch(
+                "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11",
+                {
+                    method: "GET",
+                });
+            if (!responseIDs.ok) {
+                throw new Error("Failed to fetch arts IDs");
             }
-            const data = await response.json();
-            return data;
+            const dataIDs = await responseIDs.json();
+            return dataIDs.objectIDs;
+        } catch (error) {
+            if (error instanceof Error) {
+                return rejectWithValue(error.message);
+            }
+            return rejectWithValue("Unknown error");
+        }
+    })
+
+export const fetchArts = createAsyncThunk<Art[], void, { state: { arts: ArtsState }; rejectValue: string }>(
+    "arts/fetchArts",
+    async (_, { getState, rejectWithValue }) => {
+        const { ids } = getState().arts;
+        const limitIDs = ids.slice(0, 10);
+
+        try {
+            const artPromises = limitIDs.map(async (id) => {
+                const responseArt = await fetch(
+                    `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
+                );
+                if (!responseArt.ok) {
+                    throw new Error(`Failed to fetch art with ID ${id}`);
+                }
+                const dataArt = await responseArt.json();
+                return {
+                    id: dataArt.objectID,
+                    title: dataArt.title,
+                    artist: dataArt.artistDisplayName,
+                    description: dataArt.creditLine,
+                    imageUrl: dataArt.primaryImage,
+                } as Art;
+            });
+
+            const artsResult = await Promise.all(artPromises);
+            return artsResult;
         } catch (error) {
             if (error instanceof Error) {
                 return rejectWithValue(error.message);
@@ -42,6 +84,18 @@ const ArtsSlice = createSlice({
     reducers: {},
     extraReducers: builder => {
         builder
+            .addCase(fetchArtsIDs.pending, (state) => {
+                state.isLoading = true;
+                state.error = null;
+            })
+            .addCase(fetchArtsIDs.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.ids = action.payload;
+            })
+            .addCase(fetchArtsIDs.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload || "Unknown error";
+            })
             .addCase(fetchArts.pending, (state) => {
                 state.isLoading = true;
                 state.error = null;
